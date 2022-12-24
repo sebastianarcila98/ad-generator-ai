81c6820cff591d05fe3751c0083963882d507722
Sebastian Arcila <sebastian.arcila@myfuelmaster.com>
2022-12-24 11:49:22 -0500

    ad generator app

---
+++ app.py
@@ -0,0 +1,32 @@
+import os
+
+import openai
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        product_description = request.form["animal"]
+        personality = request.form["personality"]
+        response = openai.Completion.create(
+            model="text-davinci-002",
+            prompt=generate_prompt(product_description, personality),
+            temperature=0.6,
+        )
+        return redirect(url_for("index", result=response.choices[0].text))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
+
+
+def generate_prompt(product_description, personality):
+    print(personality, product_description)
+    return """Write a persuasive four sentence product description aimed at someone who is {} based on the following product description:
+    product description: {} 
+    persuasive description: """.format(
+        personality, product_description
+    )
